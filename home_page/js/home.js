d1c22f96405a31ee7c9a1da48cd477e30e7bb915
VAGNER OLIVEIRA <wagneroliveiroficial@gmail.com>
2024-11-30 23:58:12 +0000

    finesg menu hamburguer

--- home_page/js/home.js
+++ home_page/js/home.js
@@ -5,6 +5,12 @@ function toogleMenu(event) {
     const nav = document.querySelector(".nav-link");
     nav.classList.toggle("active");
 }
+function toogleMenu2(event) {
+    if(event.type === "touchstart") event.preventDefault();
+    btn_hamburguer.classList.toggle("active");
+}
 
 btn_hamburguer.addEventListener("click", toogleMenu);
+btn_hamburguer.addEventListener("click", toogleMenu2);
 btn_hamburguer.addEventListener("touchstart", toogleMenu);
+btn_hamburguer.addEventListener("touchstart", toogleMenu2);
