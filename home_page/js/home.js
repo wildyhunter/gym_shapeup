a2d005c62c555b9ae75a807a7bb8bbb42e0ad98c
VAGNER OLIVEIRA <wagneroliveiroficial@gmail.com>
2024-11-30 22:32:52 +0000

    update

--- home_page/js/home.js
+++ home_page/js/home.js
@@ -1,8 +1,10 @@
 const btn_hamburguer = document.querySelector(".btn-hamburger");
 
-function toogleMenu() {
+function toogleMenu(event) {
+    if(event.type === "touchstart") event.preventDefault();
     const nav = document.querySelector(".nav-link");
     nav.classList.toggle("active");
 }
 
 btn_hamburguer.addEventListener("click", toogleMenu);
+btn_hamburguer.addEventListener("touchstart", toogleMenu);
